480161671ba4a5507589731a3b7d620e30e501d0
Md. Hasibul Islam <45915540+hasib999@users.noreply.github.com>
2023-01-28 15:24:20 +0600

    Data Fetch Done

--- country_app/src/App.js
+++ country_app/src/App.js
@@ -1,9 +1,40 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+import React,{useState,useEffect} from "react";
+
+const url = "https://restcountries.com/v3.1/all";
 
 function App() {
-  return (
-    <div className="App">
+  const [isLoading,setIsLoading] = useState(true);
+  const [error,setError] = useState(null);
+  const [countries,setCountries] = useState([]);
 
-    </div>
+  const fetchData = async (url) => {
+    setIsLoading(true);
+    try{
+      const response = await fetch(url);
+      const data = await response.json();
+      setCountries(data);
+      setIsLoading(false);
+      setError(null);
+    }
+    catch(error){
+      setIsLoading(false);
+      setError(error);
+    }
+  }
+
+
+  useEffect(()=>{
+    fetchData(url)
+  },[])
+
+  return (
+    <>
+      <h1>Country App</h1>
+      {isLoading && <h2>Loading...</h2>}
+      {error && <h2>{error.message}</h2>}
+    </>
   );
 }
 
